28049f2a4cfef526b45f4c90cb25b82d5c4ad41e
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-04-13 00:50:17 -0300

    Code Organization

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,31 @@
+// TODO: Criar uma função separada para mostrar mensagem de envio reservado
+
+// ========================= GLOBAL VARIABLES ==========================
+// =====================================================================
+
+// =========================== AUX FUNCTIONS ===========================
+function getSidebar() {
+  return document.querySelector(".sidebar");
+}
+
+function getSidebarBackground() {
+  return document.querySelector(".sidebar-background");
+}
+
+function getSelectedContact() {
+  return document.querySelector(".contact.selected");
+}
+
+function getSelectedMethod() {
+  return document.querySelector(".method.selected");
+}
+
+function getPrivateMessageInformation() {
+  return document.querySelector("footer span");
+}
+// =====================================================================
+
+// ===================== EVENT LISTENER FUNCTIONS ======================
 function login(event) {
   if (event.type !== "click" && event.key !== "Enter") return;
 
@@ -5,62 +33,58 @@ function login(event) {
   loginDiv.classList.add("hidden");
 }
 
-function getSideBar() {
-  const sidebar = document.querySelector(".sidebar");
-  const sidebarBackground = document.querySelector(".sidebar-background");
-
-  return {
-    bar: sidebar,
-    background: sidebarBackground,
-  };
-}
-
 function showSidebar() {
-  const sidebar = getSideBar();
-
-  sidebar.bar.classList.remove("hidden");
-  sidebar.background.classList.remove("hidden");
+  const sidebar = getSidebar();
+  const sidebarBackground = getSidebarBackground();
+  sidebar.classList.remove("hidden");
+  sidebarBackground.classList.remove("hidden");
 }
 
 function hideSidebar() {
-  const sidebar = getSideBar();
-
-  sidebar.bar.classList.add("hidden");
-  sidebar.background.classList.add("hidden");
+  const sidebar = getSidebar();
+  const sidebarBackground = getSidebarBackground();
+  sidebar.classList.add("hidden");
+  sidebarBackground.classList.add("hidden");
 }
 
-function getSelectedContact() {
-  return document.querySelector(".contact.selected");
-}
-
-function selectContact(element) {
+function selectContact(newSelected) {
   const selected = getSelectedContact();
+  const newSelectedText = newSelected.querySelector("h4").innerHTML;
+  const selectedMethodText = getSelectedMethod().querySelector("h4").innerHTML;
+
+  if (newSelectedText === "Todos" && selectedMethodText === "Reservadamente") return;
 
   selected.classList.remove("selected");
-  element.classList.add("selected");
+  newSelected.classList.add("selected");
+
+  changePrivateMessageInformation(selectedMethodText, newSelectedText);
 }
 
-function getPrivateMessageInformation() {
-  return document.querySelector("footer span");
+function selectMethod(newSelected) {
+  const selected = getSelectedMethod();
+  const newSelectedText = newSelected.querySelector("h4").innerHTML;
+  const selectedContactText = getSelectedContact().querySelector("h4").innerHTML;
+
+  if (selectedContactText === "Todos" && newSelectedText === "Reservadamente") return;
+
+  selected.classList.remove("selected");
+  newSelected.classList.add("selected");
+
+  changePrivateMessageInformation(newSelectedText, selectedContactText);
 }
+// =====================================================================
 
-// TODO: Criar uma função separada para mostrar mensagem de envio reservado
-
-function selectMethod(element) {
-  const selected = document.querySelector(".method.selected");
-  const method = element.querySelector("h4");
+// ======================= MAIN FUNCTIONALITIES ========================
+function changePrivateMessageInformation(method, contact) {
   const privateMessageInformation = getPrivateMessageInformation();
-  const selectedContact = getSelectedContact().querySelector("h4").innerHTML;
 
-  selected.classList.remove("selected");
-  element.classList.add("selected");
-
-  if (method.innerHTML === "Reservadamente") {
-    privateMessageInformation.querySelector(".name").innerHTML = selectedContact;
+  if (method === "Reservadamente") {
+    privateMessageInformation.firstElementChild.innerHTML = contact;
     privateMessageInformation.classList.remove("hidden");
   }
 
-  if (method.innerHTML === "Público") {
+  if (method === "Público") {
     privateMessageInformation.classList.add("hidden");
   }
 }
+// =====================================================================
